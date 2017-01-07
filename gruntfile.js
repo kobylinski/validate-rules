852fdc34e33345341573175cf2846577d4cdeed0
Marek Kobyliński <marek@niepsuj.it>
2017-01-07 07:57:46 +0100

    fix

--- gruntfile.js
+++ gruntfile.js
@@ -2,11 +2,6 @@ module.exports = function(grunt){
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.initConfig({
 		uglify: {
-			options: {
-				compress: {
-					drop_console: true
-				},
-			},
 			build:{
 				files: {
 					'validate-rules.min.js': ['validate-rules.js']
